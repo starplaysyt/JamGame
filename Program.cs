b57792638e51c6c0643395082e52dad7db467440
starplaysyt <horaudmitry@gmail.com>
2025-08-08 13:47:26 +0300

    structure basics implemented

--- Program.cs
+++ Program.cs
@@ -9,10 +9,22 @@ namespace JamGame
         public static void Main(string[] args)
         {
             SDL_Init(SDL_INIT_EVERYTHING);
-            SDL_ttf.TTF_Init();
-            SDL_image.IMG_Init(SDL_image.IMG_InitFlags.IMG_INIT_JPG);
 
-            Console.WriteLine(SDL_GetError());
+            bool isRunning = true;
+            
+            IntPtr window = SDL_CreateWindow("testwindow", 
+                SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                200, 200, SDL_WindowFlags.SDL_WINDOW_SHOWN | SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
+
+            while (isRunning)
+            {
+                while (SDL_PollEvent(out var evt) != 0)
+                {
+                    //Event handling block
+                }
+                
+                //Rendering block
+            }
         }
     }
 }
