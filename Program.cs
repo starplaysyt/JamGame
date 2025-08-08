504d6cac916f9f72f65a945db293d78ed3b97fba
starplaysyt <horaudmitry@gmail.com>
2025-08-08 13:27:44 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SDL2;
+using static SDL2.SDL;
+
+namespace JamGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SDL_Init(SDL_INIT_EVERYTHING);
+            SDL_ttf.TTF_Init();
+            SDL_image.IMG_Init(SDL_image.IMG_InitFlags.IMG_INIT_JPG);
+
+            Console.WriteLine(SDL_GetError());
+        }
+    }
+}
+
